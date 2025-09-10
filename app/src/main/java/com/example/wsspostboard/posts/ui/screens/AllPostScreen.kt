628893cce2f255d3f94b370a15f6b04c16a2924d
artishinde939 <artishinde939@gmail.com>
2025-09-11 01:57:42 +0530

    feat: UI issue

--- app/src/main/java/com/example/wsspostboard/posts/ui/screens/AllPostScreen.kt
+++ app/src/main/java/com/example/wsspostboard/posts/ui/screens/AllPostScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -91,7 +92,7 @@ fun AllPostsScreen(
                             color = Color.White, fontFamily = montserratFontFamily,
                             style = MaterialTheme.typography.titleMedium,
                             modifier = Modifier
-                                .fillMaxWidth(), textAlign = TextAlign.Center,
+                                .fillMaxWidth().offset(x = (-9).dp), textAlign = TextAlign.Center,
                             fontWeight = FontWeight.Medium,
                             fontSize = 16.sp
                         )
